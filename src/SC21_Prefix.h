b8468104f606b3c0d42171289f4515e6916e44e4
Marius Kintel <kintel@sim.no>
2004-03-17 12:16:16 +0000

    Renamed SC21_Prefix.h to SC21_Prefix.pch

--- src/SC21_Prefix.h
+++
@@ -1,8 +0,0 @@
-//
-// Prefix header for all source files of the 'SC21' target in the 'SC21' project
-//
-
-#ifdef __OBJC__
-    #import <Cocoa/Cocoa.h>
-    #import <InterfaceBuilder/InterfaceBuilder.h>
-#endif
