a4e25d0c5e4dd638b0e3141b4de1852f0377103f
Marius Kintel <kintel@sim.no>
2004-04-05 19:50:42 +0000

    *** empty log message ***

---
+++ src/SCCursors.m
@@ -0,0 +1,87 @@
+/* ============================================================== *
+ |                                                                |
+ | This file is part of SC21, a Cocoa user interface binding for  |
+ | the Coin 3D visualization library.                             |
+ |                                                                |
+ | Copyright (c) 2003 Systems in Motion. All rights reserved.     |
+ |                                                                |
+ | SC21 is free software; you can redistribute it and/or          |
+ | modify it under the terms of the GNU General Public License    |
+ | ("GPL") version 2 as published by the Free Software            |
+ | Foundation.                                                    |
+ |                                                                |
+ | A copy of the GNU General Public License can be found in the   |
+ | source distribution of SC21. You can also read it online at    |
+ | http://www.gnu.org/licenses/gpl.txt.                           |
+ |                                                                |
+ | For using Coin with software that can not be combined with the |
+ | GNU GPL, and for taking advantage of the additional benefits   |
+ | of our support services, please contact Systems in Motion      |
+ | about acquiring a Coin Professional Edition License.           |
+ |                                                                |
+ | See http://www.coin3d.org/mac/SC21 for more information.       |
+ |                                                                |
+ | Systems in Motion, Bygdoy Alle 5, 0257 Oslo, Norway.           |
+ |                                                                |
+ * ============================================================== */
+
+#import "SCCursors.h"
+#import "SCView.h"
+
+@implementation NSCursor (SCCursors)
+
++ (NSCursor *)rotateCursor
+{
+  static NSCursor * rotateCursor = nil;
+  if (!rotateCursor) {
+    NSBundle * frameworkBundle = [NSBundle bundleForClass:[SCView class]];
+    if (frameworkBundle) {
+      NSString * imagePath = [frameworkBundle pathForResource:@"curs_rotate"
+                                              ofType:@"tiff"];
+      NSImage * img = [[NSImage alloc] initWithContentsOfFile:imagePath];
+      NSPoint hotspot = {8, 8};
+      rotateCursor = [[NSCursor alloc] initWithImage:img hotSpot:hotspot];
+      if (rotateCursor) NSLog(@"  rotateCursor created.");
+      else NSLog(@"  creation of rotateCursor failed.");
+    }
+  }
+  return rotateCursor;
+}
+
++ (NSCursor *)zoomCursor
+{
+  static NSCursor * zoomCursor = nil;
+  if (!zoomCursor) {
+    NSBundle * frameworkBundle = [NSBundle bundleForClass:[SCView class]];
+    if (frameworkBundle) {
+      NSString * imagePath = [frameworkBundle pathForResource:@"curs_zoom"
+                                              ofType:@"tiff"];
+      NSImage * img = [[NSImage alloc] initWithContentsOfFile:imagePath];
+      NSPoint hotspot = {8, 8};
+      zoomCursor = [[NSCursor alloc] initWithImage:img hotSpot:hotspot];
+      if (zoomCursor) NSLog(@"  zoomCursor created.");
+      else NSLog(@"  creation of zoomCursor failed.");
+    }
+  }
+  return zoomCursor;
+}
+
++ (NSCursor *)panCursor
+{
+  static NSCursor * panCursor = nil;
+  if (!panCursor) {
+    NSBundle * frameworkBundle = [NSBundle bundleForClass:[SCView class]];
+    if (frameworkBundle) {
+      NSString * imagePath = [frameworkBundle pathForResource:@"curs_pan"
+                                              ofType:@"tiff"];
+      NSImage * img = [[NSImage alloc] initWithContentsOfFile:imagePath];
+      NSPoint hotspot = {8, 8};
+      panCursor = [[NSCursor alloc] initWithImage:img hotSpot:hotspot];
+      if (panCursor) NSLog(@"  panCursor created.");
+      else NSLog(@"  creation of panCursor failed.");
+    }
+  }
+  return panCursor;
+}
+
+@end
