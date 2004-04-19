14185378986bd3f61f4343690afd29d8aa462b80
Marius Kintel <kintel@sim.no>
2004-04-19 22:34:52 +0000

    Reorganized pkg creation, meta package support

--- packaging/scripts/makesc21pkg.sh
+++ packaging/scripts/makesc21pkg.sh
@@ -26,7 +26,7 @@ if test $VERBOSE; then
   set -x
 fi
 
-if test -z $SRCROOT -o -z $DSTROOT -o -z $TARGET_BUILD_DIR -o -z $PRODUCT_NAME; then
+if test -z "$SRCROOT" -o -z "$DSTROOT" -o -z "$TARGET_BUILD_DIR" -o -z "$PRODUCT_NAME"; then
   echo "Error: This script should be run from Xcode."
   exit 1
 fi
@@ -36,6 +36,6 @@ if test x$VERBOSE = x; then
   REDIRECT='> /dev/null 2>&1'
 fi
 #FIXME: Path is different on Jaguar
-eval "/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -p $TARGET_BUILD_DIR/$PRODUCT_NAME -f $DSTROOT -r $SRCROOT/packaging/pkgresources -i $SRCROOT/packaging/SC21_Info.plist -d $SRCROOT/packaging/SC21_Description.plist $REDIRECT"
+eval "/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -p \"$TARGET_BUILD_DIR/$PRODUCT_NAME\" -f \"$PRODUCT_ROOT\" -r \"$SRCROOT/$RESOURCE_DIR\" -i \"$SRCROOT/$INFOPLIST_FILE\" -d \"$SRCROOT/$DESCRIPTION_PLIST_FILE\" $REDIRECT"
 
 echo "PackageMaker returned $?"
