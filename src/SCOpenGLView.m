7aad70a28117208c4ecb55c172513ea9fe690b7e
Marius Kintel <kintel@sim.no>
2004-04-30 00:00:21 +0000

    retain/release fixes, doc update

--- src/SCOpenGLView.m
+++ src/SCOpenGLView.m
@@ -11,12 +11,12 @@
 
  @implementation SCOpenGLView
 
- /*!  
+ /*"  
    Designated initializer.
 
    Initializes a newly allocated NSOpenGLView with frameRect as its
    frame rectangle and format as its pixel format.
- */
+ "*/
  - (id)initWithFrame:(NSRect)frameRect pixelFormat:(SCOpenGLPixelFormat *)format
  {
    NSLog(@"SCOpenGLView.initWithFrame:pixelFormat");
@@ -43,16 +43,16 @@
    [[NSNotificationCenter defaultCenter] removeObserver:self];
 
    [self clearGLContext];
-   [_pixelformat release];
+   [self setPixelFormat:nil];
    [super dealloc];
  }
 
 
- /*!
-   Will create and return a default pixel format:
+ /*"
+   Returns a default #SCOpenGLPixelFormat.
    Loops through an internal prioritized list of pixel format requirements
    and selects the first valid pixelformat found.
- */
+ "*/
  + (SCOpenGLPixelFormat *)defaultPixelFormat
  {
    NSLog(@"SCOpenGLView.defaultPixelFormat");
@@ -101,24 +101,25 @@
  #endif
  }
 
-/*!
-  Will set the pixelformat for this view.
+/*"
+  Sets the receiver's #SCOpenGLPixelFormat to pixelFormat
   
   FIXME: Should we force a recreation of our OpenGL context?
   Test how NSOpenGLView behaves and document this
   (kintel 20040456)
-*/
+"*/
 - (void)setPixelFormat:(SCOpenGLPixelFormat *)pixelFormat
 {
   NSLog(@"SCOpenGLView.setPixelFormat");
 
-  [_pixelformat autorelease];
-  _pixelformat = [pixelFormat retain];
+  [pixelFormat retain];
+  [_pixelformat release];
+  _pixelformat = pixelFormat;
 }
 
-/*!
-  Returns the current pixelformat.
-*/
+/*"
+  Returns the #SCOpenGLPixelFormat associated with the receiver.
+"*/
 - (SCOpenGLPixelFormat *)pixelFormat
 {
   NSLog(@"SCOpenGLView.pixelFormat");
@@ -126,22 +127,24 @@
   return _pixelformat;
 }
 
-/*!
+/*"
   Used by subclassers to initialize OpenGL state. This function is called
   once after an OpenGL context is created and the drawable is attached.
   
   This function is called from within NSOpenGLContext.
-*/
+
+  //FIXME: This probably only works under Panther (kintel 20040429)
+"*/
 - (void)prepareOpenGL
 {
   NSLog(@"SCOpenGLView.prepareOpenGL");
 }
 
-/*!
+/*"
   Releases the NSOpenGLContext associated with the receiver. If
   necessary, this method calls clearDrawable on the context before
   releasing it.
-*/
+"*/
 - (void)clearGLContext
 {
   NSLog(@"SCOpenGLView.clearGLContext");
@@ -153,13 +156,13 @@
   }
 }
 
-/*!
+/*"
   Returns the NSOpenGLContext associated with the receiver. If the
   receiver has no associated context, a new NSOpenGLContext is created
   and returned. The new NSOpenGLContext is initialized with the
   receiver's -pixelFormat. If this function returns nil,
   a new pixelformat is created using +defaultPixelFormat.
-*/
+"*/
 - (NSOpenGLContext *)openGLContext
 {
   if (!_openGLContext) {
@@ -178,21 +181,25 @@
 }
 
 
-/*!
+/*"
   Sets the NSOpenGLContext used by the receiver to allow sharing the
   same context on a per view basis. Replaces the existing context if
   one was already created. You must call setView: on the context to
   sync the context with the view.
-*/
+"*/
 - (void)setOpenGLContext:(NSOpenGLContext *)context
 {
   NSLog(@"SCOpenGLView.setOpenGLContext");
 
-  [self clearGLContext];
-  _openGLContext = [context retain];
+  [context retain];
+  if (_openGLContext) {
+    if ([_openGLContext view] == self) [_openGLContext clearDrawable];
+    [_openGLContext release];
+  }
+  _openGLContext = context;
 }
 
-/*!
+/*"
   Called if the visible rectangle or bounds of the receiver change
   (for scrolling or resize). The default implementation does
   nothing. Override this method if you need to adjust the viewport and
@@ -204,19 +211,19 @@
 
   FIXME: Should we make sure that we have a valid context before calling reshape?
   FIXME: Should we call reshape after creating a context (i.e. after prepareOpenGL) ?
-*/
+"*/
 - (void)reshape
 {
 }
 
-/*!
+/*"
   Called if the view's context needs to be updated because the window
   moves, or if the view moves or is resized. This method simply calls
   NSOpenGLContext's update. Override this method if you need to add
   locks for multithreaded access to multiple contexts.
 
   -update is called whenever the view changes size or location.
-*/
+"*/
 - (void)update
 {
   if ([_openGLContext view] == self) [_openGLContext update];
@@ -224,18 +231,18 @@
 
 // Overridden methods from parent
 
-/*!
-  We are opaque; clearing the drawing rectangle.
-*/
+/*"
+  Overridden from NSView: we are always drawing every pixel.
+"*/
 - (BOOL)isOpaque
 {
   return YES;
 }
 
-/*!
-  Overridden from parent to make sure that we have a current context
+/*"
+  Overridden from NSView to ensure that we have a valid context
   bound to this view.
-*/
+"*/
 - (void)lockFocus
 {
   // NSLog(@"SCOpenGLView.lockFocus");
