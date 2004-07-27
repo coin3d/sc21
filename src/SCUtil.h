bfb2ca39ea158204f06e5b6e7178cc4846a2fbdc
Marius Kintel <kintel@sim.no>
2004-07-27 16:19:06 +0000

    Added SC21_LOG_METHOD, removed #undef SC21_DEBUG

--- src/SCUtil.h
+++ src/SCUtil.h
@@ -1,4 +1,10 @@
-#undef SC21_DEBUG 
+//FIXME: [self class] always return the actual Class of the self object.
+//       We want the class currently in scope. How?
+#ifdef SC21_DEBUG
+#define SC21_LOG_METHOD NSLog(@"%@.%@", [self class], NSStringFromSelector(_cmd))
+#else
+#define SC21_LOG_METHOD ((void) 0)
+#endif
 
 #ifdef SC21_DEBUG
 #undef SC21_DEBUG
@@ -9,3 +15,4 @@
 
 // #define SC21_ENV_DEBUG(_SC_env, _SC_str)              \
 // if (_SC_env environment variable is set) NSLog(_SC_str)
+
