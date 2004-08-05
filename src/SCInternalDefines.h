8922b08e75d81befd4098262ae7b3e2afbcc1939
Marius Kintel <kintel@sim.no>
2004-08-05 11:47:45 +0000

    Moved internal stuff to separate header

--- src/SCInternalDefines.h
+++ src/SCInternalDefines.h
@@ -25,18 +25,8 @@
  |                                                                 |
  * =============================================================== */
 
-#import <Sc21/SCInternalDefines.h>
-
-/*" The run-time Sc21 version number "*/
-SC21_EXTERN const double Sc21VersionNumber;
-
-// Version numbers are increased by the following scheme:
-// o Micro versions (e.g. 1.0.1): changes only after the decimal point
-// o Minor versions (e.g. 1.1.0): increase by some number (might be >1 if
-//                                there were intermediate beta releases)
-// o Major versions:              basically the same as for minor versions
-//                                but the Letter version will also change
-//                                (e.g. A -> B).
-/*" FIXME: Document "*/
-#define Sc21VersionNumber1_0 3
-//FIXME: or: #define SC21_VERSION_1_0 3
+#ifdef __cplusplus
+#define SC21_EXTERN		extern "C"
+#else
+#define SC21_EXTERN		extern
+#endif
